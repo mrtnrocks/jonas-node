84eed3fa53343d3db892975177cbf7ab70a2d34b
mrtn.codes <mrtn.glch@tuta.io>
2019-10-12 16:10:45 +0200

    create web server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+
+////////////////////////////////////////
+// FILES
+
 
 // Blocking Synchronous Way
 //const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -8,20 +13,32 @@ const fs = require('fs');
 //console.log('File written');
 
 // Non-Blocking Async Way
-fs.readFile('./txt/start.txt', 'utf-8',  (err, data1) => {
-    if (err) return console.log('ERROR');
+//fs.readFile('./txt/start.txt', 'utf-8',  (err, data1) => {
+//    if (err) return console.log('ERROR');
+//
+//    fs.readFile(`./txt/${data1}.txt`, 'utf-8',  (err, data2) => {
+//        console.log(data2);
+//        fs.readFile('./txt/append.txt', 'utf-8',  (err, data3) => {
+//            console.log(data3);
+//
+//            // write all data in one file
+//            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+//                
+//                console.log('The file has been saved!');
+//              });
+//          });
+//      });
+//  });
+//  console.log('Will read file!'); 
 
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8',  (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8',  (err, data3) => {
-            console.log(data3);
+////////////////////////////////////////
+// SERVER
 
-            // write all data in one file
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
-                
-                console.log('The file has been saved!');
-              });
-          });
-      });
-  });
-  console.log('Will read file!');
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server!')
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+})
