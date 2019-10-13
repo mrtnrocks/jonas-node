9911b9b7b3385d326fdbc4e4aa8c331220e26ac8
mrtn.codes <mrtn.glch@tuta.io>
2019-10-13 13:01:29 +0200

    routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 ////////////////////////////////////////
 // FILES
@@ -35,10 +36,21 @@ const http = require('http');
 // SERVER
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!')
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>')
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
-})
+});   
