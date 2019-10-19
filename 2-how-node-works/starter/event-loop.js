e7c865364bcb1bcd88deb5286e1de4ab64a94b28
mrtn.codes <mrtn.glch@tuta.io>
2019-10-19 11:57:54 +0200

    event loop in practice

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -9,6 +9,7 @@ setImmediate(() => console.log('Immediate 1 finished'));
 
 fs.readFile('test-file.txt', () => {
     console.log('I/O finished');
+    console.log('-------------');
 
     setTimeout(() => console.log('Timer 1 finished'), 0);
     setTimeout(() => console.log('Timer 3 finished'), 3000);
