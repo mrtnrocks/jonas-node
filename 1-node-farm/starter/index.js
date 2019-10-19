f2cfd6999a2cb43ba09c17dc8a60a06d72f82faf
mrtn.codes <mrtn.glch@tuta.io>
2019-10-19 11:56:55 +0200

    event loop in practice

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -36,19 +36,32 @@ const url = require('url');
 // SERVER
 
 // Top level code --> runs once before servers starts --> sync is ok
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview page
     if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        res.end(tempOverview);
+
+    // Product page
     } else if (pathName === '/product') {
         res.end('This is PRODUCT');
+
+    // API    
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
+    
+    // Not found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
